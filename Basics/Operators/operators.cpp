e4c8e9a0af73b5cbd0fdaaf5a68f1de1c18a6b5c
Muhammad ArslaN shahzaD <masmbdin2000@gmail.com>
2021-05-10 23:35:09 +0500

    Day 2 of 100 operators

---
+++ Basics/Operators/operators.cpp
@@ -0,0 +1,29 @@
+/*
+ ----------------------- Day 2 ----------------------
+ ---  operators  --------------
+ --- Date: 10.15.2020---------------------------------
+ Author: Muhammad Arslan Shahzad, 
+ ------------------------------------------------------
+*/
+#include <iostream>
+using namespace std;
+
+int main(void){
+    
+   
+
+
+    return 0;
+}
+
+
+/*
+Sample Output
+
+    1st run 
+   
+    2nd run
+   
+    3rd run  
+   
+*/ 
