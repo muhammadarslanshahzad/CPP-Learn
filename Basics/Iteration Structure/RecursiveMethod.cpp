24b42b86e1bcc01c1b2b3515e58f4d5329f5642f
Muhammad ArslaN shahzaD <masmbdin2000@gmail.com>
2021-05-10 22:06:32 +0500

    Day2 of100 recursion functions

---
+++ Basics/Iteration Structure/RecursiveMethod.cpp
@@ -0,0 +1,49 @@
+/*
+ ----------------------- Day 2 ----------------------
+ ---  Function Recursion  --------------
+ --- Date: 10.15.2020---------------------------------
+ Author: Muhammad Arslan Shahzad, 
+ ------------------------------------------------------
+*/
+#include <iostream>
+using namespace std;
+
+int fact(int a){
+    if(a>1){
+        return a * fact(a-1);
+    }
+    return 1;
+}
+
+
+int main(void){
+    
+    int x;
+    cout<<"Enter the number you want to calculate the factorial : ";
+    cin>>x;
+
+    int f=fact(x);
+    cout<<"factorial of numbe is : "<<f<<endl; 
+    return 0;
+}
+
+
+/*
+Sample Output
+        
+    1st run 
+
+    Enter the number you want to calculate the factorial : 7
+    factorial of numbe is : 5040
+    
+    2nd run 
+
+    Enter the number you want to calculate the factorial : 12
+    factorial of numbe is : 479001600
+
+    3rd run 
+    
+    Enter the number you want to calculate the factorial : 5
+    factorial of numbe is : 120
+
+*/ 
