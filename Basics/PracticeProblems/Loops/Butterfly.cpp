0aaf6a81e8a00d25774c9ef24317c2e79967b615
Muhammad ArslaN shahzaD <masmbdin2000@gmail.com>
2021-05-12 05:02:47 +0500

    Day 3 of 100 diamond and butterfly

---
+++ Basics/PracticeProblems/Loops/Butterfly.cpp
@@ -0,0 +1,64 @@
+/*
+ ----------------------- Day 3 ----------------------
+ -----  Butterfly pattern   -----------------------------------
+ ----- Date: 11.15.2020---------------------------------
+ Author: Muhammad Arslan Shahzad, 
+ ------------------------------------------------------
+*/
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int row;
+    cout<<"Enter number of rows : ";
+    cin>>row;
+
+    for(int i=1; i<=row; i++){
+        for(int j=1; j<=i;j++){
+            cout<<"*";
+        }
+        int space=2*row-2*i;
+        for(int j=1; j<=space; j++){
+            cout<<" ";
+        }
+        for(int j=1; j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    for(int i=row; i>=1; i--){
+        for(int j=1; j<=i;j++){
+            cout<<"*";
+        }
+        int space=2*row-2*i;
+        for(int j=1; j<=space; j++){
+            cout<<" ";
+        }
+        for(int j=1; j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+
+
+    return 0;
+}
+
+/*
+    Enter number of rows : 6
+    *          *
+    **        **
+    ***      ***
+    ****    ****
+    *****  *****
+    ************
+    ************
+    *****  *****
+    ****    ****
+    ***      ***
+    **        **
+    *          *
+  
+*/
---
+++ Basics/PracticeProblems/Loops/holloDiamond.cpp
@@ -0,0 +1,70 @@
+/*
+ ----------------------- Day 3 ----------------------
+ -----  Diamond Pattern  -----------------------------------
+ --- Date: 11.15.2020---------------------------------
+ Author: Muhammad Arslan Shahzad, 
+ ------------------------------------------------------
+*/
+#include <iostream>
+using namespace std;
+
+int main(void){
+   int r,j;
+    cout<<"Enter number of Rows : ";
+    cin>>r;
+
+    int space= (2*r-1)/2;
+    for(int i=1; i<=r;i++){
+        for(j=1; j<=space; j++){
+            cout<<" ";
+        }
+        for(j=1; j<=(2*i-1); j++){
+            if(j==1 || j==2*i-1){
+                cout<<"*";
+            } else{
+                cout<<" ";
+            } 
+        }
+        cout<<endl;
+        space--;
+    }
+    space=0;
+    for(int i=r; i>=1; i--){
+        for(j=1; j<=space; j++){
+            cout<<" ";
+        }
+        for(j=1; j<=(2*i-1); j++){
+            if(j==1 || j==2*i-1){
+                cout<<"*";
+            } else{
+                cout<<" ";
+            } 
+        }
+        cout<<endl;
+        space++;
+    }  
+    return 0;
+}
+
+/*
+Sample Output
+
+Enter number of Rows : 8
+       *       
+      * *      
+     *   *     
+    *     *    
+   *       *   
+  *         *  
+ *           * 
+*             *
+*             *
+ *           *
+  *         *
+   *       *
+    *     *
+     *   *
+      * *
+       *
+
+*/
