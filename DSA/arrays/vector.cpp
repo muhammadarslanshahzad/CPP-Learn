e220e391260ce1757335489cd136dc09bac5fb02
Muhammad ArslaN shahzaD <masmbdin2000@gmail.com>
2021-05-29 22:59:22 +0500

    d21/100 vectors and basic implemetation of vectors

---
+++ DSA/arrays/vector.cpp
@@ -0,0 +1,53 @@
+/*
+ ----------------------- Day 21 ----------------------
+ ----- VEctor     ----------
+ --- Date: 29.05.2021---------------------------------
+ Author: Muhammad Arslan Shahzad, 
+ ------------------------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(void){
+
+    vector<int> arr;
+    arr.push_back(10);
+    arr.push_back(20);
+    arr.push_back(30);
+
+//printing vextor using iterator
+    vector<int>::iterator it;
+    for(it=arr.begin(); it!=arr.end(); it++){
+       cout<<*it<<endl;
+      }
+
+    arr.pop_back();
+
+    //printing using loop only
+    for(int i=0; i<arr.size(); i++){
+        cout<<endl<<arr[i]<<endl;
+    }
+
+
+    return 0;
+}
+
+/**
+ * Sample Output 
+ * 
+ *  10
+    20
+    30
+ * 
+ * 
+ *  10
+    20
+    30
+
+    10
+    20
+ * */ 
