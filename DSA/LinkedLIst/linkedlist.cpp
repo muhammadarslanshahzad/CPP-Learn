16b09da7390f1e9c8d8336956340b7b91deae344
Muhammad ArslaN shahzaD <masmbdin2000@gmail.com>
2021-05-31 20:12:29 +0500

    d23/100 LL functions

--- DSA/LinkedLIst/linkedlist.cpp
+++ DSA/LinkedLIst/linkedlist.cpp
@@ -9,6 +9,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//node structure
 class node {
     public:
         int data;
@@ -21,28 +22,54 @@ class node {
     private:
 };
 
+//inserting
 void insertAtTail (node* &head, int val){
     node* n = new node(val);
     if(head==NULL){
         head=n;
+        return;
     }
 
     node* temp = head;
-    while(temp->next!=NULL){
+    while(temp->next != NULL){
         temp = temp->next;
     }
     temp->next=n;
 }
 
+//display
 void display(node* head){
     node* temp =head;
     while(temp!=NULL){
-        cout<<temp->data<<" ";
+        cout<<temp->data<<" -> ";
         temp=temp->next;
     }
-    cout<<endl;
+    cout<<"NULL"<<endl;
+}
 
+//deletion
 
+void deleteAtHead(node * &head){
+    node * deletehead = head;
+    head = head->next;
+    delete deletehead;
+}
+
+void deletion(node* &head, int val){
+    if(head==NULL){
+        return;
+    }
+    if(head->next == NULL){
+        deleteAtHead(head);
+        return;
+    }
+    node * temp = head;
+    while( temp->next->data != val){
+        temp=temp->next;
+    }
+    node* deletenode = temp->next;
+    temp->next = temp->next->next;
+    delete deletenode;
 }
 
 int main(void){
@@ -55,6 +82,14 @@ int main(void){
     insertAtTail(head, 5);
     insertAtTail(head, 6);
     insertAtTail(head, 7);
+    insertAtTail(head, 8);
+    insertAtTail(head, 9);
+
+    display(head);
+    
+    deletion(head, 9);
+    deletion(head, 1);
+    deletion(head, 8);
 
     display(head);
     
