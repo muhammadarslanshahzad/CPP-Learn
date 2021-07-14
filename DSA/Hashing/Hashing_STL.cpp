96e3d5f412a38455d5c671bfbf449097a68847a0
masmbdin2000 <masmbdin2000@gmail.com>
2021-07-14 22:25:15 +0500

    Day67 Hashing

---
+++ DSA/Hashing/Hashing_STL.cpp
@@ -0,0 +1,31 @@
+ /*
+ ----------------------- Day 67 ----------------------
+ ----- Hashing in STL      ----------
+ --- Date: 14.07.2021---------------------------------
+ Author: Muhammad Arslan Shahzad, 
+ ------------------------------------------------------
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(void){
+
+    map<int, int> m;
+
+    m[8] =2;
+    m[10]=12;
+    m[6]=20;
+    m[15]=12;
+    cout<<m[8]<<" "<<m[6]<<" "<<m[12]<<endl;
+
+ 
+    return 0;
+}
+
+/**
+ * Sample Output 
+ * 
+ * 2 20 0
+ * */ 
